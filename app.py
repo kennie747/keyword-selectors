f78185fe90826ce025d448563e7984be165eff49
Moses Aregbesola <123170893+mkaregbesola@users.noreply.github.com>
2023-09-11 10:01:16 +0100

    modified code to also send context

--- app.py
+++ app.py
@@ -8,13 +8,13 @@ app = Flask(__name__)
 api_key = os.environ.get("ENV_VARIABLE_NAME")
 
 # Function to generate text using the GPT-3.5 Turbo engine
-def generate_text(prompt):
+def generate_text(prompt, context):
     openai.api_key = api_key
 
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",  # "gpt-4", #Use the GPT-3.5 Turbo model
         messages=[
-            {"role": "system", "content": "You are a professional tech blogging assistant."},
+            {"role": "system", "content": context}, ### context NEW, initially "You are a professional tech blogging assistant."
             {"role": "user", "content": prompt}
             #{"role": "user", "content": "Generate a list of 10 HIGHLY RELEVANT Key-phrases (BETWEEN 2 TO 4 words long) for the previous post, sort the list in descending order of relevance and label it *Keywords*, and make it comma separated with NO nunbering"}
         ]
@@ -27,13 +27,15 @@ def generate_text(prompt):
 def api_generate_text():
     data = request.get_json()
     prompt = data.get('prompt')
+    context = data.get('context') ### NEW ###
 
-    if not prompt:
-        return jsonify({"error": "Missing 'prompt' parameter"}), 400
+    if not prompt or not context: ###  or not context NEW ###
+        return jsonify({"error": "Missing 'prompt' or 'context' parameter"}), 400 ### or 'context' NEW ###
 
     prompt = f"\"{prompt}\"." # Formatted to make it easy to insert any preceeding text as shown the comment below
     # f"Please rewrite the following tech blog post in a unique and original way while preserving all the information and key points; if there are images in the original post, retain them and add appropriate attribution; conclude with a list of 10 HIGHLY RELEVANT Key-phrases (BETWEEN 2 TO 4 words long) for the post, sorted in descending order of relevance and label the list *Keywords*, comma separated with NO nunbering: \"{text}\"."
-    generated_text = generate_text(prompt)
+    context = f"\"{context}\"." ### NEW ###
+    generated_text = generate_text(prompt, context) ### ,context NEW ###
 
     return jsonify({"generated_text": generated_text})
 
